f1b09b8a72fbfaf141fc0cf06547fa261017610f
anliben <anliben.yayoi@gmail.com>
2024-02-13 11:24:54 -0300

    :sparkles: aula de estruturas

--- main.go
+++ main.go
@@ -2,31 +2,23 @@ package main
 
 import "fmt"
 
-const nome = "Gopher"
-
-func Welcome() {
-	fmt.Println("Initializing the application... ")
+type Configuracoes struct {
+	Porta   int    `json:"porta"`
+	Host    string `json:"host"`
+	Usuario string `json:"usuario"`
+	Senha   string `json:"senha"`
+	Banco   string `json:"banco"`
 }
 
 func main() {
 
-	Welcome()
-
-	for i := 0; i < 10; i++ {
-		fmt.Println("Hello, World! ", i)
+	configs := Configuracoes{
+		Porta:   3306,
+		Host:    "localhost",
+		Usuario: "root",
+		Senha:   "root",
+		Banco:   "dev",
 	}
 
-	// Examples of primitive data types
-	var number int = 10
-	var pi float64 = 3.14
-	var isTrue bool = true
-	var character byte = 'A'
-	var text string = "Hello, World!"
-
-	fmt.Println(number)
-	fmt.Println(pi)
-	fmt.Println(isTrue)
-	fmt.Println(character)
-	fmt.Println(text)
-	fmt.Println("meu nome e ", nome)
+	fmt.Println(configs)
 }
